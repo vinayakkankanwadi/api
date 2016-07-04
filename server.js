eb8998581548062f55b2e2e4a354f365cb8bd84f
Vinayak Kankanwadi <email>
2016-07-05 04:59:36 +1000

    GET POST support for user added using mongodb

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // Packages we require
 // --------------------------------
 var express = require('express');
-
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var User = require('./models/user');
 
 // Create our Express application
 var app = express();
@@ -10,15 +12,52 @@ var app = express();
 var router = express.Router();
 
 // --------------------------------
-// Register routers
+// Register routers, body-parser
 // --------------------------------
+// NOTE Order of bodyParser and router matters
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
 app.use('/',router);
 
-// Get
+
+// GET
 router.get('/',function(req,res){
 	res.json({message: 'Getting resposne from Vin'})
+
 });
 
+//USER
+var userRoute = router.route('/users');
+
+//GET
+userRoute.get(function(req,res){
+	User.find(function(err,users){
+		if(err)
+			res.send(err);
+		res.json(users);
+	});
+});
+
+// POST
+
+userRoute.post(function(req,res){
+	//console.log('POST',req.body.name,req.body.id);
+	var user = new User();
+	user.name = req.body.name;
+	user.id = req.body.id;
+	
+	user.save(function(err){
+		if(err)
+			res.send(err);
+		res.json({message:'User added', data:user })
+	});	
+});
+
+// --------------------------------
+//Connect to users MongoDB
+// --------------------------------
+mongoose.connect('mongodb://localhost:27017/users')
 
 // Start the server on the port
 var port = 3000;
