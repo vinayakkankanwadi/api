385e8667772112ed73ff318cdc8edd5b97cb6bd9
Vinayak Kankanwadi <email>
2016-07-07 07:42:19 +1000

    added oauth2 authorization

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var ejs = require('ejs');
+var session = require('express-session');
 var dataController = require('./controllers/data');
 var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
+var clientController = require('./controllers/client');
+var oauth2Controller = require('./controllers/oauth2');
 
 // --------------------------------
 //Connect to data MongoDB
@@ -16,6 +20,10 @@ mongoose.connect('mongodb://localhost:27017/api')
 
 // Create our Express application
 var app = express();
+
+// Set View engine to ejs
+app.set('view engine','ejs')
+
 // Create Router
 var router = express.Router();
 
@@ -34,6 +42,20 @@ router.route('/data/:id')
 router.route('/user')
 	.post(userController.postUsers)
 	.get(authController.isAuthenticated,userController.getUsers);	
+
+// create endpoint handler for /client
+router.route('/client')
+	.post(authController.isAuthenticated,clientController.postClients)
+	.get(authController.isAuthenticated,clientController.getClients);
+	
+// create endpoint handler for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+// create endpoint handler for oauth2 token
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
 	
 // --------------------------------
 // Register routers, body-parser
@@ -42,6 +64,15 @@ router.route('/user')
 app.use(bodyParser.urlencoded({
 	extended:true
 }));
+
+
+// Use express session support since OAuth2orize requires it
+app.use(session({
+  secret: 'Super Secret Session Key',
+  saveUninitialized: true,
+  resave: true
+}));
+
 //Use passport 
 app.use(passport.initialize());
 
